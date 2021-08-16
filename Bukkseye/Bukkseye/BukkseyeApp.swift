523b7dfd2d801867b86ad3ed8af81e4a688e3362
Julia Jimenez <maggie.jh99@gmail.com>
2021-08-16 17:59:02 -0500

    added alerts

---
+++ Bukkseye/Bukkseye/BukkseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BukkseyeApp.swift
+//  Bukkseye
+//
+//  Created by Maggie Jimenez Herrera on 16/08/21.
+//
+
+import SwiftUI
+
+@main
+struct BukkseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bukkseye/Bukkseye/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  Bukkseye
+//
+//  Created by Maggie Jimenez Herrera on 16/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var alertIsVisible: Bool = false
+    @State private var sliderValue: Double = 50.0
+    
+    var body: some View {
+        VStack {
+            Text("🎯🎯🎯 \nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text("89")
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            HStack {
+                Text("1")
+                    .bold()
+                Slider(value: self.$sliderValue, in: 1.0...100.0)
+                Text("100")
+                    .bold()
+            }
+            Button(action: {
+                print("Hello, SwiftUI!")
+                self.alertIsVisible = true
+            }) {
+                Text("Hit me")
+            }
+            .alert(isPresented: self.$alertIsVisible, content: {
+                return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(Int(self.sliderValue))."), dismissButton: .default(Text("Awsome!")))
+            })
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
+    }
+}
