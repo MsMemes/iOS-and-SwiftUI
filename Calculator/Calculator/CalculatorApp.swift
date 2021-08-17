45710f9d37753bf871adbe416f35ba564f6a0066
Julia Jimenez <maggie.jh99@gmail.com>
2021-08-17 13:42:15 -0500

    Started calculator

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Maggie Jimenez Herrera on 17/08/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
